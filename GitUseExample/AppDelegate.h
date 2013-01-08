aa771c0734c5d13058ecb338cf9967d1ff3dc7cd
DERIC KRAMER <merlin910@DERICs-MacBook-Air.local>
2013-01-08 14:42:36 -0700

    Initial Commit

---
+++ GitUseExample/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  GitUseExample
+//
+//  Created by DERIC KRAMER on 1/8/13.
+//  Copyright (c) 2013 Kramer Performance Tech. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ GitUseExample/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  GitUseExample
+//
+//  Created by DERIC KRAMER on 1/8/13.
+//  Copyright (c) 2013 Kramer Performance Tech. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
